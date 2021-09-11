2fced5222404562401837089f709ccf684ddbb4f
Falak <45658281+FalakChhikara@users.noreply.github.com>
2021-09-11 18:38:54 +0530

    Delete backend directory

--- backend/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport');
-
-const home_controller = require('../controllers/home_controller');
-
-router.post('/',passport.checkAuthentication,home_controller.home);
-
-module.exports = router;
